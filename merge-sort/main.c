65b6700dd5898f4efcbc048891911e8b5a035d23
luciomelo1407 <luciomelo90@gmail.com>
2025-10-08 13:53:05 -0300

    problema do split do merge sort

---
+++ merge-sort/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void printVec(int* vector, int size){
+  for(int i = 0; i<size;i++ ){
+    if(i+1<size){
+      printf("%d, ",vector[i]);
+    }else{
+      printf("%d\n",vector[i]);
+    }
+  }
+}
+
+
+
+
+int* mergeSort(int* vector, int size ){
+  printVec(vector,size);
+  if(size>1){
+
+    int middle = size/2;
+    
+    if(size%2){
+      mergeSort(vector,middle);
+      mergeSort(vector+middle, middle+1);
+    }else{
+      mergeSort(vector,middle);
+      mergeSort(vector+middle, middle);
+    }
+  }
+  
+  return vector;
+}
+
+
+
+
+int main(int argc, char *argv[])
+{
+  int vector[3] = {2,5,6};
+  
+
+  return 0;
+}
