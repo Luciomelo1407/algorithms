5c2a0d405885f5f4ee5a419bd622341a8d271bc2
luciomelo1407 <luciomelo90@gmail.com>
2026-01-06 11:30:50 -0300

    corrections in qq sort

--- quick-sort/main.c
+++ quick-sort/main.c
@@ -2,126 +2,120 @@
 #include <stdlib.h>
 #include <time.h>
 
-void printVec(int* vector, int size){
-  for(int i = 0; i < size;i++ ){
-    if(i+1<size){
-      printf("%d, ",vector[i]);
-    }else{
-      printf("%d\n",vector[i]);
+void printVec(int *vector, int size) {
+  for (int i = 0; i < size; i++) {
+    if (i + 1 < size) {
+      printf("%d, ", vector[i]);
+    } else {
+      printf("%d\n", vector[i]);
     }
   }
 }
 
-int lomuto_default(int* vector, int begin, int end) {
-    int pivot = vector[end];
-    int x = begin - 1;
-    int swap = 0;
+int lomuto_default(int *vector, int begin, int end) {
+  int pivot = vector[end];
+  int x = begin - 1;
+  int swap = 0;
 
-    for (int y = begin; y < end; y++) {
-        if (vector[y] <= pivot) {
-            x++;
-            swap = vector[x];
-            vector[x] = vector[y];
-            vector[y] = swap;
-        }
+  for (int y = begin; y < end; y++) {
+    if (vector[y] <= pivot) {
+      x++;
+      swap = vector[x];
+      vector[x] = vector[y];
+      vector[y] = swap;
     }
+  }
 
-    swap = vector[x + 1];
-    vector[x + 1] = vector[end];
-    vector[end] = swap;
+  swap = vector[x + 1];
+  vector[x + 1] = vector[end];
+  vector[end] = swap;
 
-    return x ;
+  return x + 1;
 }
 
-int lomuto_median(int* vector, int begin, int end, int median){
-    if (begin == end){
+int lomuto_median(int *vector, int begin, int end, int median) {
+  if (begin == end) {
     return begin;
-  }else{
+  } else {
     int pivot = lomuto_default(vector, begin, end);
-    if(pivot - begin + 1 >= median) {
-      return lomuto_median(vector,begin, pivot,median);
-    }
-    else{
-      return lomuto_median(vector, pivot+1, end, median - (pivot - begin + 1));
+    if (pivot - begin + 1 >= median) {
+      return lomuto_median(vector, begin, pivot, median);
+    } else {
+      return lomuto_median(vector, pivot + 1, end,
+                           median - (pivot - begin + 1));
     }
   }
- 
 }
 
-int lomuto_rand(int* vector, int begin, int end){
-  int swap = vector[end], r_index = begin + rand()%(end - begin +1);
+int lomuto_rand(int *vector, int begin, int end) {
+  int swap = vector[end], r_index = begin + rand() % (end - begin + 1);
   vector[end] = vector[r_index];
   vector[r_index] = swap;
-  return lomuto_default(vector, begin,  end);
+  return lomuto_default(vector, begin, end);
 }
 
-
-
-int hoare_default(int* vector, int begin, int end){
+int hoare_default(int *vector, int begin, int end) {
   int pivot = vector[begin], x = begin - 1, y = end + 1, swap = 0;
   while (1) {
     while (vector[--y] > pivot) {
     }
     while (vector[++x] < pivot) {
     }
-    if(x<y){
+    if (x < y) {
       swap = vector[x];
       vector[x] = vector[y];
       vector[y] = swap;
-    }else{
+    } else {
       return y;
     }
   }
 }
 
-int hoare_rand(int* vector, int begin, int end){
-  int swap = vector[begin], r_index = begin + rand()%(end - begin +1);
+int hoare_rand(int *vector, int begin, int end) {
+  int swap = vector[begin], r_index = begin + rand() % (end - begin + 1);
   vector[begin] = vector[r_index];
   vector[r_index] = swap;
-  return hoare_default(vector,begin, end);
+  return hoare_default(vector, begin, end);
 }
 
-int hoare_median(int* vector, int begin, int end, int median){
-  if (begin == end){
+int hoare_median(int *vector, int begin, int end, int median) {
+  if (begin == end) {
     return begin;
-  }else{
+  } else {
     int pivot = hoare_default(vector, begin, end);
-    if(pivot - begin + 1 >= median) {
-      return hoare_median(vector,begin, pivot,median);
-    }
-    else{
-      return hoare_median(vector, pivot+1, end, median - (pivot - begin + 1));
+    if (pivot - begin + 1 >= median) {
+      return hoare_median(vector, begin, pivot, median);
+    } else {
+      return hoare_median(vector, pivot + 1, end, median - (pivot - begin + 1));
     }
   }
 }
 
-
-void quick_sort(int* vector, int begin, int end){
-  if(begin < end){
-    // int pivot = hoare_default(vector, begin, end);
-    int pivot = lomuto_median(vector, begin, end, (end - begin)/2);
-    quick_sort(vector,begin,pivot);
-    quick_sort(vector,pivot+1,end);
+void quick_sort(int *vector, int begin, int end) {
+  if (begin < end) {
+    int pivot = hoare_default(vector, begin, end);
+    // int pivot = lomuto_median(vector, begin, end, (end - begin) / 2);
+    quick_sort(vector, begin, pivot);
+    quick_sort(vector, pivot + 1, end);
   }
 }
 
-int* generateVector(int size){
+int *generateVector(int size) {
   srand(time(NULL));
-  int* vector = malloc(size*sizeof(int));
-  for(int i = 0; i < size; i++){
+  int *vector = malloc(size * sizeof(int));
+  for (int i = 0; i < size; i++) {
     vector[i] = rand() % 100;
   }
   return vector;
 }
 
-int main(int argc, char *argv[])
-{
-  int* vector = generateVector(10);
+int main(int argc, char *argv[]) {
+  int *vector = generateVector(10);
   printf("not ordered: ");
   printVec(vector, 10);
   quick_sort(vector, 0, 9);
   printf("ordered vector: ");
-  printVec(vector,10);
-  
+  printVec(vector, 10);
+
   return 0;
 }
